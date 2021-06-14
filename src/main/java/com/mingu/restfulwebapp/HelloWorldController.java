5be92fd622ef5265a02274f3a50b41c3b3a214d7
mangojuicedeli <mgkang0317@gmail.com>
2021-06-14 12:43:04 +0900

    feat : hello world api 생성

---
+++ src/main/java/com/mingu/restfulwebapp/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.mingu.restfulwebapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world");
+    }
+}
