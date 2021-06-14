b5a5ef68b4aeef28cc6eae3a4782ba589943c9e7
mangojuicedeli <mgkang0317@gmail.com>
2021-06-14 13:10:36 +0900

    feat : hello world api - pathVariable 사용 api 생성

--- src/main/java/com/mingu/restfulwebapp/HelloWorldController.java
+++ src/main/java/com/mingu/restfulwebapp/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.mingu.restfulwebapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello world");
+        return new HelloWorldBean("Hello world, Bean");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world, %s", name));
     }
 }
