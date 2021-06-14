a0b436b2436e77cf1172fadabdbc539ecd00f9db
mangojuicedeli <mgkang0317@gmail.com>
2021-06-14 11:43:53 +0900

    first commit

---
+++ src/test/java/com/mingu/restfulwebapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingu.restfulwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
