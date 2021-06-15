01853d32274ce3612bd99849f21888329a4eba4f
mangojuicedeli <mgkang0317@gmail.com>
2021-06-15 15:59:13 +0900

    feat : 응답코드 제어 및 aop를 이용한 예외 핸들링

--- src/main/java/com/mingu/restfulwebapp/user/UserController.java
+++ src/main/java/com/mingu/restfulwebapp/user/UserController.java
@@ -1,7 +1,10 @@
 package com.mingu.restfulwebapp.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,11 +23,23 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        // 찾는 user가 없을 경우, null이 아닌 예외를 던져서 200 응답이 나오지 않게 한다.
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+        // 응답 헤더에 생성된 유저의 id를 path에 넣어서 location 이라는 데이터로 전달한다.
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        // 유저가 정상 생성된 경우, 기본 성공 코드인 200이 아닌 create(201)로 엄밀하게 응답 코드를 지정한다.
+        return ResponseEntity.created(location).build();
     }
 }
---
+++ src/main/java/com/mingu/restfulwebapp/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mingu.restfulwebapp.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// 그냥 exception만 던지면 5XX 결과코드로 응답하므로, not_found(404) 코드로 에러를 특정해서 응답한다
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
