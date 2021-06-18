9b954e14dedd0dfd270e19a9380c8e3c64f7b4e8
mangojuicedeli <mgkang0317@gmail.com>
2021-06-18 12:18:30 +0900

    feat: swagger를 통한 api documentation

--- src/main/java/com/mingu/restfulwebapp/user/User.java
+++ src/main/java/com/mingu/restfulwebapp/user/User.java
@@ -3,6 +3,8 @@ package com.mingu.restfulwebapp.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,21 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password"})
 //@JsonFilter("UserInfo") // UserInfo 라는 이름의 필터를 추가
+@ApiModel(description = "사용자 정보 도메인 객체")
 public class User {
 
     private Integer id;
     @Size(min = 2, message = "이름은 2글자 이상 입력해야합니다.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해주세요.")
     private Date joinDate;
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 비밀번호를 입력해주세요.")
     private String password;
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
 
 }
