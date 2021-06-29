d9225f968234870650c38c1f47a79dcaa9597a72
mangojuicedeli <mgkang0317@gmail.com>
2021-06-29 15:12:34 +0900

    feat: h2 db 및 jpa - 전체 사용자 조회 추가

--- src/main/java/com/mingu/restfulwebapp/user/User.java
+++ src/main/java/com/mingu/restfulwebapp/user/User.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -19,8 +22,11 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password"})
 //@JsonFilter("UserInfo") // UserInfo 라는 이름의 필터를 추가
 @ApiModel(description = "사용자 정보 도메인 객체")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "이름은 2글자 이상 입력해야합니다.")
     @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'user1', 'test1', '701010-1111111');
+insert into user values (2, sysdate(), 'user2', 'test2', '801010-1111111');
+insert into user values (3, sysdate(), 'user3', 'test3', '901010-1111111');
