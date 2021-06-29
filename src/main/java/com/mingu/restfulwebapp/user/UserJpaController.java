9bae686a85fd2abc8cf1c1b2a8454a7c463ba81d
mangojuicedeli <mgkang0317@gmail.com>
2021-06-29 15:43:04 +0900

    feat: jpa - 개별 사용자 조회 추가(HATEOAS 적용)

--- src/main/java/com/mingu/restfulwebapp/user/UserJpaController.java
+++ src/main/java/com/mingu/restfulwebapp/user/UserJpaController.java
@@ -1,11 +1,18 @@
 package com.mingu.restfulwebapp.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -16,5 +23,22 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        /*
+        HATEOAS
+         */
+        EntityModel entityModel = EntityModel.of(user);
+        // 개별 사용자 조회에서 할 수 있는 추가 작업으로 Link 객체에 '전체 사용자 조회' uri를 넣어준다.
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        // Link 객체를 만든 후, entityModel에 all-users 라는 이름으로 추가한다.
+        entityModel.add(linkTo.withRel("all-users"));
+        return entityModel;
     }
 }
